6d86e995ec71572e37625471f0b563f938ef989c
heis-divine <79201528+heis-divine@users.noreply.github.com>
2021-07-08 09:30:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+# # Flow chart 1
+#
+# name = input("What is your name?")
+# if name=="Alice":
+#     print("Hi,Alice")
+#
+# # Flow chart 2
+# name1 = input("What's your name?")
+# if name1=="Alice":
+#     print("Hi,Alice")
+# else:
+#     print("Hello,Stranger.")
+
+#Flow chart 3
+# name2 = input("What is your name?")
+# if name2 == "Alice":
+#     print("Hello, Alice")
+# else:
+#     age = int(input('enter your age'))
+#     if age < 12:
+#         print('Your are not alice kiddo')
+#
+# # Flow chart 4
+# name3 = input("What is your name?")
+# if name3 == "Alice":
+#     print("Hello, Alice")
+# else:
+#     age = int(input('enter your age'))
+#     if age < 12:
+#         print('Your are not alice kiddo')
+#     elif age > 2000:
+#         print("Unlike you, Alice is not an undead, immortal vampire")
+#     elif age > 100:
+#         print("You are not Alice, grandma")
+#
+# # Flow Chart 5
+# name4 = input("What is your name?")
+# if name4 == "Alice":
+#     print("Hello, Alice")
+# else:
+#     age = int(input('enter your age'))
+#     if age < 12:
+#         print('Your are not alice kiddo')
+#     elif age > 100:
+#         print("You are not Alice, grandma")
+#     elif age > 2000:
+#         print("Unlike you, Alice is not an undead, immortal vampire")
+# Flow chart 6
+# name5 = input("What is your name?")
+# if name5 == "Alice":
+#     print("Hello, Alice")
+# else:
+#     age = int(input('enter your age'))
+#     if age < 12:
+#         print('Your are not alice kiddo')
+#     else:
+#         print("You are neither, Alice nor a little kid")
+
+# # Flow Chart 7
+# quest1 = input("Is it raining ")
+# if quest1.lower() == "yes":
+#     quest2 = input("Do you have an umbrella?")
+#     if quest2 == "yes":
+#         print("Go outside")
+#     elif quest2 == "no":
+#         print("Wait a while")
+#         print()
+#         quest3 = input("Is it raining?")
+#         if quest3 == "no":
+#             print("Go Outside")
+#         elif quest3 == "yes":
+#             print("wait a while")
+# elif quest1 == "no":
+#     print("Go Outside")
+# # Flow chart 8
+# spam = int(input("Enter a Number"))
+# while spam < 5:
+#     print("Hello World")
+#     spam = spam + 1
+#
+# # Flow chart 9
+# spam = int(input("Enter a Number"))
+# while spam < 5:
+#     print("Hello World")
+#     spam += 1
+#     continue
+#
+# # Flow Chart 11
+# print("My name is")
+# for i in range(5):
+#     print("Jimmy Five Times(" + str(i) + ')')
+
+
+
+
+
+
+
+
+
+
+
